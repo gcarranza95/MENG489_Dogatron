d56ffe129e13b49c34624a17e71549a8feb8fadc
gerardo.carranza <gcarranza95@gmail.com>
2016-10-26 05:44:44 -0400

    Digital Compass Code
    Provides you with the heading

---
+++ MENG489_DigitalCompass_Test/MENG489_DigitalCompass_Test.ino
@@ -0,0 +1,53 @@
+#include <Wire.h> //I2C Arduino Library
+
+#define addr 0x1E //I2C Address for The HMC5883
+
+void setup(){
+  
+  Serial.begin(9600);
+  Wire.begin();
+  
+  
+  Wire.beginTransmission(addr); //start talking
+  Wire.write(0x02); // Set the Register
+  Wire.write(0x00); // Tell the HMC5883 to Continuously Measure
+  Wire.endTransmission();
+}
+
+
+void loop(){
+  
+  int x,y,z; //triple axis data
+
+  //Tell the HMC what regist to begin writing data into
+  Wire.beginTransmission(addr);
+  Wire.write(0x03); //start with register 3.
+  Wire.endTransmission();
+  
+ 
+ //Read the data.. 2 bytes for each axis.. 6 total bytes
+  Wire.requestFrom(addr, 6);
+  if(6<=Wire.available()){
+    x = Wire.read()<<8; //MSB  x for reference on bitshift see: https://www.arduino.cc/en/Reference/Bitshift
+    x |= Wire.read(); //LSB  x for reference on bitwise OR see: https://www.arduino.cc/en/Reference/BitwiseAnd
+    z = Wire.read()<<8; //MSB  z
+    z |= Wire.read(); //LSB z
+    y = Wire.read()<<8; //MSB y
+    y |= Wire.read(); //LSB y
+  }
+
+  
+  // Show Values
+  Serial.print("X: "); Serial.print(x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(z); Serial.print("  ");
+
+  float heading = atan2(y,x);
+  if(heading<0)
+  heading += 2*M_PI;
+  float headingDegrees = heading * 180/M_PI; 
+  Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
+  
+  
+  delay(500);
+}
